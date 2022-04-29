9d5709f5e26669ba50da5737690c007ae9a85c34
Serhiy Holovin <sergiusz.holovin@gmail.com>
2022-04-29 14:26:59 +0300

    Added working test of IndexedDB

--- extension/background.js
+++ extension/background.js
@@ -27,48 +27,40 @@ function colorSelectedText(){
         range.insertNode(replacement)
     }
 
-    // insertToDb()
+    insertToDb()
 
-    // function insertToDb(){
-    //        //! indexedDB part
-    // window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
-    // window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction;
-    // window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange;
-    // if(window.indexedDB){console.log("Db created")}else {console.log("db is not supported")}
+    function insertToDb(){
+           //! indexedDB part
+    window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
+    window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction;
+    window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange;
+    if(window.indexedDB){console.log("Db created")}else {console.log("db is not supported")}
 
     
-    // let request = window.indexedDB.open("QuizQuestDatabase" , 1),db,tx,store,indexedDB
+    let request = window.indexedDB.open("QuizQuestDatabase" , 1),db,tx,store,indexedDB
 
-    // request.onupgradeneeded = fucntion(e) {
-    //     let db = request.result
-    //     store = db.createObjectStore("QuestionStore" , {keyPath:"qID"})
-    //     index = store.createInxdex("questionText" , "questionText" , {unique:false})
-    // }
+    request.onupgradeneeded = function(e) {
+        let db = request.result
+        store = db.createObjectStore("QuestionStore" , {keyPath:"qID"})
+        index = store.createIndex("questionText" , "questionText" , {unique:false})
+    }
 
-    // request.onerror = function(e){console.log("There was an error: " + e.target)}
+    request.onerror = function(e){console.log("There was an error: " + e.target)}
 
-    // request.onsuccess = fucntion (e) {
-    //     db = request.result
-    //     tx = db.transaction("QuestionStore","readwrite")
-    //     store = tx.objectStore("QuestionStore")
-    //     index = store.index("questionText")
+    request.onsuccess = function (e) {
+        db = request.result
+        tx = db.transaction("QuestionStore","readwrite")
+        store = tx.objectStore("QuestionStore")
+        index = store.index("questionText")
 
-    //     db.onerror = function(e){console.log("error with the db onsuccess: " + e.target)}
-    //     store.put({qID: 1 , questionText:"The sky is blue",correctAnswer:true , studentAnswer:true, result:true })
-    //     store.put({qID: 2 , questionText:"The grass is green",correctAnswer:true , studentAnswer:true, result:true })
-    // }
+        db.onerror = function(e){console.log("error with the db onsuccess: " + e.target)}
+        store.put({qID: 1 , questionText:"The sky is blue",correctAnswer:true , studentAnswer:true, result:true })
+        store.put({qID: 2 , questionText:"The grass is green",correctAnswer:true , studentAnswer:true, result:true })
+    }
 
-    // tx.oncomplete = fucntion()
-    // {
-    //     db.close()
-    // }
-    
+    tx.oncomplete = ()=>
+    {
+        db.close()
+    }
+  } //end of insert into DB
 }
-     
-                                                
-
-
-
-   
-
-
